c202ea8d185e9b70f0300a7ba07ad37eea51e176
Bin Matsui <binmatsui@hotmail.com>
2021-11-20 23:28:06 +0900

    ボタンを押したらChromeを起動

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,21 @@
+using OpenQA.Selenium.Chrome;
+
 namespace ConnpassAutomator
 {
     public partial class Form1 : Form
     {
+        private ChromeDriver Driver { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            Driver = new ChromeDriver(baseDir, new ChromeOptions(), TimeSpan.FromSeconds(120));
+            Driver.Url = "https://connpass.com/editmanage/";
         }
     }
 }
