4eb65b0d16b994dc2d8ba1bd7d210609306ee167
Bin Matsui <binmatsui@hotmail.com>
2021-11-20 23:56:03 +0900

    ユーザー名、パスワード入力 終了時自動保存

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 42);
+            this.button1.Location = new System.Drawing.Point(53, 221);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 34);
             this.button1.TabIndex = 0;
@@ -41,20 +43,42 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(53, 96);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(150, 31);
+            this.userNameTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(53, 154);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(150, 31);
+            this.passwordTextBox.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userNameTextBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private TextBox userNameTextBox;
+        private TextBox passwordTextBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using ConnpassAutomator.Properties;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace ConnpassAutomator
@@ -18,6 +20,22 @@ namespace ConnpassAutomator
             {
                 Url = "https://connpass.com/editmanage/"
             };
+            Driver.FindElement(By.Name("username")).SendKeys(userNameTextBox.Text);
+            Driver.FindElement(By.Name("password")).SendKeys(passwordTextBox.Text);
+            Driver.FindElement(By.Id("login_form")).Submit();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default.UserName = userNameTextBox.Text;
+            Settings.Default.Password = passwordTextBox.Text;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userNameTextBox.Text = Settings.Default.UserName;
+            passwordTextBox.Text = Settings.Default.Password;
         }
     }
 }
