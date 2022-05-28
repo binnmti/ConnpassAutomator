dd0728a66cc0a21dbff3facf5b529b0cdcf2500f
Bin Matsui <binmatsui@hotmail.com>
2022-05-29 00:20:31 +0900

    SettingManagerを作る

---
+++ ConnpassAutomator/SettingManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnpassAutomator
+{
+    /// <summary>
+    /// セーブ、ロードの責務を持つ
+    /// </summary>
+    public class SettingManager
+    {
+        public static ConnpassWillbeRenamed Load()
+        {
+            return new ConnpassWillbeRenamed();
+        }
+
+        public static void Save(ConnpassWillbeRenamed connpassWillbeRenamed)
+        {
+        }
+    }
+}
